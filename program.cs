4bb22dbe7a73cd34e1d44012fee5ca339e28162f
CMD <gautamkailashsingh@gmail.com>
2020-09-29 13:32:10 +0530

    [Gautam] Added a loop to enable the player to reach 100

--- program.cs
+++ program.cs
@@ -7,13 +7,21 @@ class program
 		int startPoint = 0;
 		Console.WriteLine("Single player will start from origin");
 		Random ran=new Random();
-		int roll=ran.Next(1,7);
-		int option = ran.Next(1,4);
-		if(option==2)
-			startPoint+=roll;
-		else if(option==3)
-			startPoint-=roll;
-		if(startPoint<0)
-			startPoint=0;
+		int count=0;
+		while (startPoint != 100)
+		{
+			int roll=ran.Next(1,7);
+			int option = ran.Next(1,4);
+			count++;
+			if(option==2)
+				startPoint+=roll;
+			else if(option==3)
+				startPoint-=roll;
+			if(startPoint<0)
+				startPoint=0;
+			if(startPoint>100)
+				startPoint-=roll;
+		}
+		Console.WriteLine("The number of times the die was rolled: "+count);
 	}
 }
