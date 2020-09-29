5295e7eca7438372ecd9322d2987daab4417213c
CMD <gautamkailashsingh@gmail.com>
2020-09-29 13:00:52 +0530

    [Gautam] Added the number of players with their starting point

--- program.cs
+++ program.cs
@@ -3,5 +3,12 @@ class program
 	static void main(String args[])
 	{
 		Console.WriteLine("Welcome to Snake and Ladder Problem");
+		start();
+	}
+	static void start()
+	{
+		int numOfPlayers = 1;
+		int startPoint = 0;
+		Console.WriteLine("Single player will start from origin");
 	}
 }
