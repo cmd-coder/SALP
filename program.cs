530f39477bc71efcb654154b94bfde29b4f8afb5
CMD <gautamkailashsingh@gmail.com>
2020-09-29 15:03:05 +0530

    [Gautam] Added another player to play with the first one

--- program.cs
+++ program.cs
@@ -3,26 +3,32 @@ class program
 	static void main(String args[])
 	{
 		Console.WriteLine("Welcome to Snake and Ladder Problem");
-		int numOfPlayers = 1;
-		int startPoint = 0;
-		Console.WriteLine("Single player will start from origin");
+		int numOfPlayers = 2;
+		int [] startPoint = {0,0};
+		Console.WriteLine("Two players will start from origin");
 		Random ran=new Random();
-		int count=0;
-		while (startPoint != 100)
+		while (startPoint1 != 100 && startPoint2!=100)
 		{
-			int roll=ran.Next(1,7);
-			int option = ran.Next(1,4);
-			count++;
-			if(option==2)
-				startPoint+=roll;
-			else if(option==3)
-				startPoint-=roll;
-			if(startPoint<0)
-				startPoint=0;
-			if(startPoint>100)
-				startPoint-=roll;
-			Console.Write(startPoint+" ");
+			for(int i=0;i<2;i++)
+			{
+				int roll=ran.Next(1,7);
+				int option = ran.Next(1,4);
+				if(option==2)
+					startPoint[i]+=roll;
+				else if(option==3)
+					startPoint[i]-=roll;
+				if(startPoint[i]<0)
+					startPoint[i]=0;
+				if(startPoint[i]>100)
+					startPoint[i]-=roll;
+				if(option==2)
+					i--;
+			}
 		}
-		Console.WriteLine("The number of times the die was rolled: "+count);
+		Console.WriteLine("Player 1 is at "+startPoint[0]+" and Player 2 is at "+startPoint[1]);
+		if(startPoint[0]==100)
+			Console.WriteLine("Player 1 has won");
+		else
+			Console.WriteLine("Player 2 has won");
 	}
 }
