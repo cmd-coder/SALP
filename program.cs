eeebf86ce95a970935b850fc67bdae83c94dd055
CMD <gautamkailashsingh@gmail.com>
2020-09-29 13:19:47 +0530

    [Gautam] Added options for no play, ladder and snake

--- program.cs
+++ program.cs
@@ -8,5 +8,12 @@ class program
 		Console.WriteLine("Single player will start from origin");
 		Random ran=new Random();
 		int roll=ran.Next(1,7);
+		int option = ran.Next(1,4);
+		if(option==2)
+			startPoint+=roll;
+		else if(option==3)
+			startPoint-=roll;
+		if(startPoint<0)
+			startPoint=0;
 	}
 }
