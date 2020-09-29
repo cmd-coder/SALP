271900a82f5d108d4f295a76a35e349ada602da0
CMD <gautamkailashsingh@gmail.com>
2020-09-29 13:06:51 +0530

    [Gautam] Merged two methods into one

--- program.cs
+++ program.cs
@@ -3,10 +3,6 @@ class program
 	static void main(String args[])
 	{
 		Console.WriteLine("Welcome to Snake and Ladder Problem");
-		start();
-	}
-	static void start()
-	{
 		int numOfPlayers = 1;
 		int startPoint = 0;
 		Console.WriteLine("Single player will start from origin");
